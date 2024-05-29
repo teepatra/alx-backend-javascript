e6167eebbc3a6a09a1818508d558c8291514afa5
“teepatra” <fagboretomilola@gmail.com>
2024-05-29 14:15:17 +0000

    Throw error / try catch

---
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env
+
+/*
+ * Race two promises
+*/
+
+export default function loadBalancer(chinaDownload, USDownload) {
+  return Promise.race([chinaDownload, USDownload])
+    .then((downloadData) => downloadData);
+}
